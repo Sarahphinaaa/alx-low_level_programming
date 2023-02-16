a8305a7fc47ff2db6127fd622cbc8e074f00e4ad
sarahphinaaa <oyesanyasarah94@gmail.com>
2023-02-16 15:12:28 -0800

    size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
-
 /**
- * main - prints "programming is like building a multilingual 
+ * main - prints programming is like building a multilingual 
  * puzzle, followed by a newline.
  * Return: Always 0.
  */
