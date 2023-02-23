1be7cbfdec03713550a979f014b49589ca289ba6
sarahphinaaa <oyesanyasarah94@gmail.com>
2023-02-23 08:46:54 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * print_most_numbers - checks for checks for a digit (0 through 9).
  *
  * Return: Always 0.
  */
-
 void print_most_numbers(void)
 {
 	int c;
@@ -20,5 +18,5 @@ void print_most_numbers(void)
 			}
 		}
 	}
-	_putchar('\n');
+	_putchar('\n')
 }
