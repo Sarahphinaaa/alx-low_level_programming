ded1b58e69da68be56bc13aaf7696f8f3aeffdc7
sarahphinaaa <oyesanyasarah94@gmail.com>
2023-04-10 10:24:42 -0700

    header

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,32 +5,32 @@
  * @filename: filename.
  * @letters: numbers of letters printed.
  *
- * Return: numbers of letters printed. If the function fails, return 0
+ * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int sf;
+	int fd;
 	ssize_t nrd, nwr;
 	char *buf;
 
 	if (!filename)
 		return (0);
 
-	sf = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
-	if (sf == -1)
+	if (fd == -1)
 		return (0);
 
-		buf = malloc(sizeof(char) * (letters));
-		if (!buf)
-			return (0);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
-		nrd = read(fd, buf, letters);
-		nwr = write(STDOUT_FILENO, buf, nrd);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-		close(sf);
+	close(fd);
 
-		free(buf);
+	free(buf);
 
-		return (nwr);
+	return (nwr);
 }
