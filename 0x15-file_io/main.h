17775f3e9c3d465fb96758e0cb3bc540f07469b5
sarahphinaaa <oyesanyasarah94@gmail.com>
2023-04-10 07:05:43 -0700

    main1

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,13 +1,5 @@
-
 #ifndef MAIN_H
 #define MAIN_H
-
-/*
- * File: main.h
- * Auth: Gedeon Obae Gekonge
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x14-file_io directory.
- */
 
 #include <sys/types.h>
 #include <sys/stat.h>
